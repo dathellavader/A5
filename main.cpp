ca346515e555113323f294abf34a2ae3560222ae
dathellavader <ella.henner@pcc.edu>
2025-11-03 01:50:56 +0000

    work

--- main.cpp
+++ main.cpp
@@ -38,8 +38,6 @@
 #include <iomanip>
 using namespace std;
 
-const bool debug = true;
-
 
 // Compile and test
 // g++ main.cpp && ./a.out < test.txt && rm ./a.out
@@ -49,22 +47,64 @@ const double tax = 0.07;
 const int slices_per_pizza = 8;
 const double delivery_charge = 0.2;
 
+int num_of_pizzas = 0;
 double cost_of_pizzas = 0.0;
 double total_tax = 0.0;
 double total_delivery_tax = 0.0;
+double pizzas_and_tax = 0.0;
 double total_cost = 0.0;
 
 int num_people = 0;
 double slices_per_person = 0.0;
 double pizza_cost = 0.0;
 
-char answer = "";
+char answer = ' ';
 
 cout << "Welcome to my Pizza Party Statistic Program!" << endl << endl;
-cout << "Enter the number of people, average number of slices per person, and the cost of a pizza separated by a space: " << endl << endl;
+cout << "Enter the number of people, average number of slices per person, and the cost of a pizza separated by a space: ";
 cin >> num_people;
 cin >> slices_per_person;
 cin >> pizza_cost;
+
+// num pizza
+
+double slices = num_people * slices_per_person;
+double fraction_pizzas = slices / slices_per_pizza;
+num_of_pizzas = ceil(fraction_pizzas);
+
+// cost of pizza
+
+cost_of_pizzas = num_of_pizzas * pizza_cost;
+
+// tax
+
+total_tax = tax * cost_of_pizzas;
+
+// pizza and tax
+
+pizzas_and_tax = cost_of_pizzas + total_tax;
+
+// delivery
+
+total_delivery_tax = delivery_charge * pizzas_and_tax;
+
+// total cost
+
+total_cost = pizzas_and_tax + total_delivery_tax;
+
+// display
+cout << endl;
+cout << fixed << setprecision(2);
+cout << "Number of pizzas: " << num_of_pizzas << endl;
+cout << "Cost of pizzas:  $" << cost_of_pizzas << endl;
+cout << "Tax:             $" << total_tax << endl;
+cout << "Delivery:        $" << total_delivery_tax << endl;
+cout << "Total Cost:      $" << total_cost << endl << endl;
+
+cout << "Do you want to enter more? (y/n): " << endl << endl;
+cin >> answer;
+
+
 
 
 
